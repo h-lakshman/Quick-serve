7456e8eacfa37c47590d7fa8cd9398b1e1ef1800
h-lakshman <lakshman.vaibhav007@gmail.com>
2024-11-05 17:01:38 +0530

    Added the new app into the project and setup the route

--- Backend/main/settings.py
+++ Backend/main/settings.py
@@ -28,6 +28,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework_simplejwt',
     'authentication.apps.AuthenticationConfig',
+    'quickserve.apps.QuickserveConfig',
 ]
 
 MIDDLEWARE = [
