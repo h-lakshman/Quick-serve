f5f02abab3d4d5493372b8d6c1751f88f0f24e80
Rahul <rahulust22@gmail.com>
2024-11-07 12:31:03 +0530

    Added home page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,19 +1,10 @@
-import Navbar from "./assets/components/Navbar";
-import Footer from "./assets/components/Footer";
-import CarouselComponent from "./assets/components/CarouselComponent";
-import CardSection from "./assets/components/CardSection";
+
 import "./App.css";
 
 function App() {
   return (
     <>
-      <Navbar />
       
-      <div className="main">
-        <CarouselComponent />
-        <CardSection/>
-      </div>
-      <Footer />
     </>
   );
 }
