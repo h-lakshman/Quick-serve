2ebef4257db4db4349613bbf6129f7219bd780b2
Rahul <rahulust22@gmail.com>
2024-11-09 12:02:20 +0530

    Added Signuproute

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,6 +3,7 @@ import "./App.css";
 import Home from "./assets/components/Home";
 import OneService from "./assets/components/OneService";
 import {BrowserRouter as Router,Routes,Route} from 'react-router-dom';
+import Signup from "./assets/components/Signup";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Home/>} />
           <Route path='/service/:id' element={<OneService/>} />
+          <Route path='/signup' elemnt={<Signup/>} />
         </Routes>
       </Router>
     </>
