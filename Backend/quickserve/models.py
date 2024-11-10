b97400893538a570a12e02302557e4f86b866499
h-lakshman <lakshman.vaibhav007@gmail.com>
2024-11-10 20:26:12 +0530

    Modified daysavailable model

--- Backend/quickserve/models.py
+++ Backend/quickserve/models.py
@@ -1,14 +1,14 @@
 from django.db import models
 from authentication.models import User
- 
- 
+
+
 class Category(models.Model):
     name = models.CharField(max_length=50)
- 
+
     def __str__(self) -> str:
         return self.name
- 
- 
+
+
 class Address(models.Model):
     building_name = models.CharField(max_length=64)
     street = models.CharField(max_length=30)
@@ -16,11 +16,11 @@ class Address(models.Model):
     city = models.CharField(max_length=30)
     state = models.CharField(max_length=30)
     pincode = models.CharField(max_length=8)
- 
+
     def __str__(self) -> str:
         return self.city
- 
- 
+
+
 class Service(models.Model):
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=99)
@@ -32,14 +32,14 @@ class Service(models.Model):
     address = models.OneToOneField(
         Address, on_delete=models.CASCADE)
     available_all_hours = models.BooleanField(default=True)
- 
+
     def __str__(self) -> str:
         return self.name
- 
- 
+
+
 class DaysAvailable(models.Model):
     service = models.OneToOneField(
-        Service, on_delete=models.PROTECT, primary_key=True)
+        Service, on_delete=models.CASCADE, primary_key=True)
     monday = models.BooleanField(default=False)
     tuesday = models.BooleanField(default=False)
     wednesday = models.BooleanField(default=False)
